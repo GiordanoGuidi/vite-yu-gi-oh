8c2a51d8dc366a9f2fa3530ab0860a197d283b93
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-02-05 17:06:20 +0100

    added first structure

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,40 @@
 </script>
 
 <template>
-  <h1 class="text-success">ciao</h1>
+  <div class="container">
+    <div class="row card-container">
+      <div class="pokemon-card">
+        <img src="" alt="" class="img-fluid">
+        <div>1</div>
+        <div>Bulbasaur</div>
+        <div>Grass</div>
+      </div>
+      <div class="pokemon-card">
+      </div>
+      <div class="pokemon-card">
+      </div>
+      <div class="pokemon-card">
+      </div>
+      <div class="pokemon-card">
+      </div>
+      <div class="pokemon-card">
+      </div>
+      <div class="pokemon-card">
+      </div>
+      <div class="pokemon-card">
+      </div>
+      <div class="pokemon-card">
+      </div>
+      <div class="pokemon-card">
+      </div>
+
+
+
+    </div>
+
+  </div>
 </template>
 
-<style></style>
+<style lang="scss">
+@use './assets/scss/style.scss'
+</style>
