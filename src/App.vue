fb65adf6746f7d4b246c605dd4f95ade02467be4
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-02-05 18:11:25 +0100

    added API call

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,35 @@
 <script>
+import axios from 'axios';
+const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons'
+export default {
+  name: 'App',
+  data: () => ({
+    pokèmon: []
+  })
+
+  ,
+  created() {
+    axios.get(endpoint).then(res => {
+      this.pokèmon = res.data.docs
+      console.log(this.pokèmon)
+
+    })
+  }
+
+}
 
 </script>
 
 <template>
   <div class="container">
     <div class="row card-container">
-      <div class="pokemon-card">
-        <img src="" alt="" class="img-fluid">
-        <div>1</div>
-        <div>Bulbasaur</div>
-        <div>Grass</div>
+      <div class="pokemon-card" v-for="pokèmon in pokèmon">
+        <img :src="pokèmon.imageUrl" alt="" class="img-fluid rounded-circle">
+        <div>{{ pokèmon.number }}</div>
+        <div>{{ pokèmon.name }}</div>
+        <div>{{ pokèmon.type1 }}</div>
       </div>
-      <div class="pokemon-card">
-      </div>
-      <div class="pokemon-card">
-      </div>
-      <div class="pokemon-card">
-      </div>
-      <div class="pokemon-card">
-      </div>
-      <div class="pokemon-card">
-      </div>
-      <div class="pokemon-card">
-      </div>
-      <div class="pokemon-card">
-      </div>
-      <div class="pokemon-card">
-      </div>
-      <div class="pokemon-card">
-      </div>
+
 
 
 
