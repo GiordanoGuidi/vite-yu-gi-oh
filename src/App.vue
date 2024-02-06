47e571f70e3193acbc61cabea39efd6470ba1d31
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-02-06 16:35:39 +0100

    added array type in store

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
 import axios from 'axios';
 const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons';
+const endpointType = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons/types1'
 import AppMain from './components/AppMain.vue'
 import { store } from './assets/data/store'
 
@@ -8,9 +9,14 @@ export default {
   name: 'App',
   components: { AppMain },
 
+  // ARRAY POKEMON IN STORE
   created() {
     axios.get(endpoint).then(res => {
       store.pokemon = res.data.docs
+    })
+    // ARRAY POKEMON TYPE IN STORE
+    axios.get(endpointType).then(res => {
+      store.type = res.data
     })
   }
 
@@ -20,6 +26,14 @@ export default {
 
 <template>
   <div class="container">
+    <!--HEADER-->
+    <header>
+
+
+    </header>
+
+
+    <!--MAIN-->
     <AppMain />
   </div>
 </template>
